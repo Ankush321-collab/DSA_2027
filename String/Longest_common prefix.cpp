d4032c88ec28af7d5b05f4e8eeb8a44399320a88
ankush adhikari <ankushadhikari321@gmail.com>
2025-08-07 23:51:03 +0530

    longest common prefix

---
+++ String/Longest_common prefix.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        string ans="";
+        sort(strs.begin(),strs.end());
+
+        string first=strs[0],last=strs[n-1];
+
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+                
+            }
+            ans+=first[i];
+        }
+        return ans;
+        
+    }
+int main(){
+   vector<string>strs={"flower","flow","flight"};
+   string ans=longestCommonPrefix(strs);
+   cout<<ans;
+   return 0;
+
+}
