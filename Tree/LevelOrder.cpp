b85dfb5abc998806d290bce155802f155d9f8283
ankush adhikari <ankushadhikari321@gmail.com>
2025-08-28 23:55:59 +0530

    level order

---
+++ Tree/LevelOrder.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    vector<vector<int>> ans;
+    if (!root) return ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int n = q.size();
+        vector<int> a;
+
+        for (int i = 0; i < n; i++) {
+            TreeNode* curr = q.front();
+            q.pop();
+            a.push_back(curr->val);
+            if (curr->left) q.push(curr->left);
+            if (curr->right) q.push(curr->right);
+        }
+        ans.push_back(a);
+    }
+    return ans;
+}
+
+int main() {
+    // Example tree:
+    //       3
+    //      / \
+    //     9   20
+    //        /  \
+    //       15   7
+
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    vector<vector<int>> result = levelOrderBottom(root);
+
+    for (auto& level : result) {
+        for (int val : level) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
