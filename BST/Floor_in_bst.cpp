abd052e66baf80478803932b8d4bf146ac68d6e5
ankush adhikari <ankushadhikari321@gmail.com>
2025-09-18 00:02:24 +0530

    bst statred

---
+++ BST/Floor_in_bst.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int v){
+        data=v;
+        left=right=NULL;
+    }
+};
+
+int ans(Node* root,int key){
+    int floor=-1;
+
+    while(root){
+
+        if(root->data==key){
+            floor=root->data;
+            return floor;
+
+        }
+        if(root->data<key){
+            floor=root->data;
+            root=root->right;
+        }
+        else{
+            root=root->left;
+        }
+    }
+    return floor;
+}
+
+int main(){
+    Node* root=new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(12);
+    root->left->left =new  Node(4);
+    root->left->right = new Node(8);
+    root->right->left = new Node(10);
+    root->right->right = new Node(14);
+
+    int key=7;
+
+    int res=ans(root,key);
+
+    cout<<"answer is:"<<res;
+    return 0;
+}
